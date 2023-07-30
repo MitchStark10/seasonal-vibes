a986be0ac26e7c1a0320a416a69a32b0113df1a6
Mitch Stark <mitchstark10@gmail.com>
2023-07-29 19:21:02 -0500

    fix prettier issue

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
-router.get('/api', (req, res) => {
-  res.status(200).json({ message: 'Hello from server!' });
-})
+router.get("/api", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "API request succeeded. May the force be with you." });
+});
 
-router.use(express.static(path.join(__dirname, '../client/build')));
+router.use(express.static(path.join(__dirname, "../client/build")));
 
 // All other GET requests not handled before will return our React app
-router.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+router.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 module.exports = router;
