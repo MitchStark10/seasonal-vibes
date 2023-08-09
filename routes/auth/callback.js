4201e76ec4e0fae36e3535fe772e8e6e9878391e
Mitch Stark <mitchstark10@gmail.com>
2023-08-08 21:41:07 -0500

    auth working

--- routes/auth/callback.js
+++ routes/auth/callback.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   const code = req.query.code || null;
   const state = req.query.state || null;
 
-  const authOptions = {
-    url: "https://accounts.spotify.com/api/token",
-    form: {
-      code: code,
-      redirect_uri: redirect_uri,
-      grant_type: "authorization_code",
-    },
-    headers: {
-      Authorization:
-        "Basic " +
-        Buffer.from(client_id + ":" + process.env.CLIENT_SECRET).toString(
-          "base64"
-        ),
-    },
-    json: true,
-  };
-
-  return res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  // TODO: Check the state and store the code
+  return res.redirect("http://localhost:3001/auth_success");
 });
 
 module.exports = app;
