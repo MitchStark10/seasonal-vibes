0e7a319ada19fae3cd0744c4b366653260a06fbe
Mitch Stark <mitchstark10@gmail.com>
2023-08-08 19:06:53 -0500

    prep redirect code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import './App.css';
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./App.css";
+
+const LOGIN_URL = process.env.REACT_APP_LOGIN_URL || "/login";
 
 const queryClient = new QueryClient();
 
@@ -7,15 +9,15 @@ const AppContainer = () => (
   <QueryClientProvider client={queryClient}>
     <AppContent />
   </QueryClientProvider>
-)
-
+);
 
 const AppContent = () => {
   return (
     <div>
       Hello world
+      <a href={LOGIN_URL}>Login to spotify</a>
     </div>
   );
-}
+};
 
 export default AppContainer;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+require("dotenv").config();
 
-router.get("/api", (req, res) => {
+const client_id = process.env.CLIENT_ID;
+const redirect_uri = process.env.REDIRECT_URI;
+
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+  for (var i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+};
+
+router.get("/login", function (_req, res) {
+  var state = generateRandomString(16);
+  var scope = "user-read-private user-read-email";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      new URLSearchParams({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      }).toString()
+  );
+});
+
+router.get("/api", (_req, res) => {
   res
     .status(200)
     .json({ message: "API request succeeded. May the force be with you." });
@@ -11,7 +42,7 @@ router.get("/api", (req, res) => {
 router.use(express.static(path.join(__dirname, "../client/build")));
 
 // All other GET requests not handled before will return our React app
-router.get("*", (req, res) => {
+router.get("*", (_req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
