b7e1e67e2e0f8ac29e07a4e0c5303942c4fd0500
Mitch Stark <MitchStark10@gmail.com>
2023-08-21 21:25:23 -0500

    begin brainstorming possibilities for playlist creation cron job

---
+++ cron/createScheduledPlaylist.js
@@ -0,0 +1,7 @@
+const createScheduledPlaylist = () => {
+  console.log("Beginning create scheduled playlist cron job");
+
+  // TODO: Find all saved users from the DB
+  // TODO: Loop through all saved users in the DB
+  // TODO: Create a playlist for each user
+};
