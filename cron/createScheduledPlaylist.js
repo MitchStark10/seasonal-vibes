3b9cbba11292c7925f64e5da3bb47fe84e0c7d08
Mitch Stark <MitchStark10@gmail.com>
2023-08-22 21:39:51 -0500

    beginning to work through user fetching code

--- cron/createScheduledPlaylist.js
+++ cron/createScheduledPlaylist.js
@@ -1,7 +1,9 @@
-const createScheduledPlaylist = () => {
+import { ScheduledPlaylists } from "../lib/db/ScheduledPlaylists";
+
+const createScheduledPlaylist = async () => {
   console.log("Beginning create scheduled playlist cron job");
 
-  // TODO: Find all saved users from the DB
+  const userList = new ScheduledPlaylists().findAllUsers();
   // TODO: Loop through all saved users in the DB
   // TODO: Create a playlist for each user
 };
