b8da50f7b8d6ff737c8703928380c67876b0fd5d
Mitch Stark <MitchStark10@gmail.com>
2023-08-27 14:28:01 -0500

    playlist creation code is working successfully

--- cron/createScheduledPlaylist.js
+++ cron/createScheduledPlaylist.js
@@ -6,6 +6,7 @@ export const createScheduledPlaylist = async () => {
   const userList = await new ScheduledPlaylists().findAllUsers();
 
   for (const user of userList) {
+    console.log("User found, creating playlist for: " + user.email);
     await user.createScheduledPlaylist({
       playlistName: "TBD - Name this playlist per the month",
     });
