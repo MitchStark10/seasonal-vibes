165683b8e6802ed17f70a8272501aa5d0d94a769
Mitch Stark <MitchStark10@gmail.com>
2023-08-27 14:57:13 -0500

    improve playlist name

--- cron/createScheduledPlaylist.js
+++ cron/createScheduledPlaylist.js
@@ -1,14 +1,36 @@
 import { ScheduledPlaylists } from "../lib/db/ScheduledPlaylists.js";
 
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const d = new Date();
+const currentMonthName = monthNames[d.getMonth()];
+const currentYear = d.getFullYear();
+
 export const createScheduledPlaylist = async () => {
-  console.log("Beginning create scheduled playlist cron job");
+  console.log(
+    "Beginning create scheduled playlist cron job for: ",
+    currentMonthName
+  );
 
   const userList = await new ScheduledPlaylists().findAllUsers();
 
   for (const user of userList) {
     console.log("User found, creating playlist for: " + user.email);
     await user.createScheduledPlaylist({
-      playlistName: "TBD - Name this playlist per the month",
+      playlistName: `${currentMonthName} ${currentYear}`,
     });
   }
 };
