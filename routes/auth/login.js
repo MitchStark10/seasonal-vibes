926d96528b6bc90df090433ae78f8ac04693b45a
Mitch Stark <MitchStark10@gmail.com>
2023-08-27 14:17:57 -0500

    ready to start testing playlist creation code

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -19,7 +19,7 @@ const generateRandomString = (length) => {
 
 app.get("/", async (_req, res) => {
   const state = generateRandomString(16);
-  const scope = "user-read-private user-read-email";
+  const scope = "user-read-private user-read-email playlist-modify-public";
 
   const requestState = new RequestState(state);
   const newRequestStateResponse = await requestState.persist();
