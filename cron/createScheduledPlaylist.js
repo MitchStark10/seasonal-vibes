97d17f47dd621ee625b1694d8b4a74fde91659a7
Mitch Stark <MitchStark10@gmail.com>
2023-08-27 14:00:33 -0500

    query for users succeeding

--- cron/createScheduledPlaylist.js
+++ cron/createScheduledPlaylist.js
@@ -3,9 +3,13 @@ import { ScheduledPlaylists } from "../lib/db/ScheduledPlaylists.js";
 export const createScheduledPlaylist = async () => {
   console.log("Beginning create scheduled playlist cron job");
 
-  const userList = new ScheduledPlaylists().findAllUsers();
+  const userList = await new ScheduledPlaylists().findAllUsers();
 
   for (const user of userList) {
-    await user.createScheduledPlaylist();
+    await user.createScheduledPlaylist({
+      playlistName: "TBD - Name this playlist per the month",
+    });
   }
 };
+
+createScheduledPlaylist();
