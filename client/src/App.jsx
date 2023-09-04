cb36ab148d27dca1ca9eafcf00d64fb25e35ad66
Mitch Stark <MitchStark10@gmail.com>
2023-09-04 18:09:49 -0500

    initialize routing code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { ThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header";
 import { theme } from "./components/Theme";
+import { Home } from "./pages/Home";
+import { Settings } from "./pages/Settings";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +19,12 @@ const AppContent = () => {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
