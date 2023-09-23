05267df6bba11cc3be4cbe3cae0488505954835f
Mitch Stark <MitchStark10@gmail.com>
2023-09-23 14:21:17 -0500

    style tweaks to text

--- client/src/components/NumberedListItem.tsx
+++ client/src/components/NumberedListItem.tsx
@@ -1,16 +1,26 @@
+import { styled } from "@mui/material";
 import React, { PropsWithChildren } from "react";
 
 interface Props {
   number: number;
 }
+
+const NumberedListItemContainer = styled("div")({
+  fontSize: "20px",
+});
+
+const NumberedListItemCounter = styled("span")({
+  fontWeight: "bold",
+});
+
 export const NumberedListItem: React.FC<PropsWithChildren<Props>> = ({
   number,
   children,
 }) => {
   return (
-    <div>
-      <span style={{ fontWeight: "bold" }}>{number}. </span>
+    <NumberedListItemContainer>
+      <NumberedListItemCounter>{number}. </NumberedListItemCounter>
       {children}
-    </div>
+    </NumberedListItemContainer>
   );
 };
