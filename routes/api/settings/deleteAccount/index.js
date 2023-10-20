0386dbfc36ad490e0c2d895da5b2ddc576ff7283
Mitch Stark <MitchStark10@gmail.com>
2023-10-19 19:51:13 -0500

    delete account endpoint

---
+++ routes/api/settings/deleteAccount/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.delete("/", async (_req, res) => {
+  await res.locals.quarterlyVibesUser.deleteUser();
+  res.status(200).json({ success: true });
+});
+
+export default app;
--- routes/api/settings/index.js
+++ routes/api/settings/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../../../lib/middleware/authMiddleware.js";
+import deleteUserRoute from "./deleteAccount/index.js";
 
 const app = express();
 
 app.use(authMiddleware);
+
+app.use("deleteAccount", deleteUserRoute);
 
 app.get("/", async (req, res) => {
   const settings = await res.locals.quarterlyVibesUser.getSettings();
