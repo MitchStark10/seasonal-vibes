b1f0e1d1824f2cb391fc92e47284544c1fca4f2e
Mitch Stark <MitchStark10@gmail.com>
2023-10-24 20:21:39 -0500

    mobile delete modal tweak

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -1,16 +1,17 @@
 import { Box, ModalProps, Modal as MuiModal } from "@mui/material";
 
 const boxStyles = {
+  maxWidth: "95vw",
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
   bgcolor: "background.paper",
   border: "2px solid #000",
   borderRadius: "4px",
   boxShadow: 24,
   p: 4,
+  minWidth: "280px",
 };
 
 export const Modal: React.FC<ModalProps> = ({ children, ...modalProps }) => {
