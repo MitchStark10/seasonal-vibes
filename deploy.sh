3258eb0975560812abdd7fe4ad71ad39a11667ad
Mitch Stark <MitchStark10@gmail.com>
2023-11-22 13:04:37 -0600

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 rm build.zip;
 npm run build;
-zip -r build.zip ./ -x './client/node_modules/*' -x './node_modules/*' -x '.vscode/*' -x '.git/*'
-az webapp deploy --resource-group quarterlyvibes --name seasonalvibes --src-path ./build.zip --debug;
+zip -r build.zip ./ -x '.vscode/*' -x '.git/*'
+az webapp deploy --resource-group quarterlyvibes --name seasonalvibes --src-path ./build.zip;
